59aa7d7dcc118b8ec157ba1fd3d75210257d3f8d
Sohaila Abo El-Wafa <133037749+Sohaila128@users.noreply.github.com>
2023-07-29 23:01:15 +0200

    edit map

--- js/script.js
+++ js/script.js
@@ -1,86 +1,101 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
- //  pure counter
-new PureCounter();
+document.addEventListener("DOMContentLoaded", () => {
 
-//  Initiate glightbox
-const glightbox = GLightbox({
-  selector: '.glightbox'
-});
+   const navToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+  function toggleNavbarCollapse() {
+    navbarCollapse.classList.toggle("show");
+  }
+  navToggler.addEventListener("click", toggleNavbarCollapse);
+  const ancorLinks = document.querySelectorAll(".nav-link");
+  ancorLinks.forEach((ancor) => {
+    ancor.addEventListener("click", function () {
+      if (navbarCollapse.classList.contains("show")) {
+        navbarCollapse.classList.remove("show");
+      }
+    });
+ });
 
- 
-//  Sticky header on scroll
-  const selectHeader = document.querySelector('#header');
+  //  pure counter
+  new PureCounter();
+
+  //  Initiate glightbox
+  const glightbox = GLightbox({
+    selector: ".glightbox",
+  });
+
+  //  Sticky header on scroll
+  const selectHeader = document.querySelector("#header");
   if (selectHeader) {
-    document.addEventListener('scroll', () => {
-      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    document.addEventListener("scroll", () => {
+      window.scrollY > 100
+        ? selectHeader.classList.add("sticked")
+        : selectHeader.classList.remove("sticked");
     });
   }
 
-// * Init swiper slider with 1 slide at once in desktop view
-new Swiper('.slides-1', {
+  // * Init swiper slider with 1 slide at once in desktop view
+  new Swiper(".testimonialsSlides", {
     speed: 600,
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 
   // Gallery Slider
-  new Swiper('.gallery-slider', {
+  new Swiper(".gallery-slider", {
     speed: 400,
     loop: true,
     centeredSlides: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
       640: {
         slidesPerView: 3,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
       992: {
         slidesPerView: 5,
-        spaceBetween: 20
-      }
-    }
+        spaceBetween: 20,
+      },
+    },
   });
 
+  // Animation on scroll function
+  function initAOS() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  }
+  window.addEventListener("load", () => {
+    initAOS();
+  });
 
-// Animation on scroll function 
-      function aos_init() {
-        AOS.init({
-            duration: 1000,
-            easing: 'ease-in-out',
-          once: true,
-          mirror: false
-        });
-    }
-    window.addEventListener('load', () => {
-        aos_init();
-      });
-      
 });
 
